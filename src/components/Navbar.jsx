7f21b03218ec7f3a95c8204a1fb19d6c2faafefb
Nicolas Orozco <nicolasorozcodavila@gmail.com>
2023-01-01 20:06:40 -0500

    Homepage

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,160 @@
+import Edu_Essence from "../assets/Edu_Essence.png";
+function Navbar() {
+  return (
+    <div className="navbar bg-base-100 p-3">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li tabIndex={0}>
+              <a className="justify-between bg-success text-xl font-bold mb-3">
+                Ciencias ambientales
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2 bg-black text-white">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li tabIndex={0}>
+              <a className="justify-between bg-warning text-xl font-bold">
+                Historia del Perú
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2 bg-black text-white">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">
+          <img
+            src={Edu_Essence}
+            alt="Logo of EduEssence"
+            width="50"
+            height="50"
+            class="p-1.5"
+          />
+          <p>"Aprender/Reforzar"</p>
+        </a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-2 gap-10">
+          <li tabIndex={0} className="bg-success outline text-lg font-bold">
+            <a>
+              Ciencias ambientales
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2 bg-black text-white">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+          <li tabIndex={0} className="bg-warning outline text-lg font-bold">
+            <a>
+              Historia del Perú
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2 bg-black text-white">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,38 @@
+function Home() {
+  return (
+    <>
+      <div className="flex flex-1 justify-around items-center">
+        <img src="https://pngimg.com/d/book_PNG2105.png" width="300" />
+        <h1 className="text-4xl text-center font-bold mx-4">
+          "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit..."
+        </h1>
+      </div>
+      <section className="bg-warning">
+        <h2 className="text-2xl text-center p-4 my-14 font-semibold outline-dashed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </h2>
+      </section>
+      <div className="flex flex-1 justify-around items-center">
+        <h2 className="text-3xl text-center font-bold mx-2">
+          Pellentesque ex velit, gravida vitae neque in, tempus facilisis diam.
+          Vivamus eu pretium nisl. Phasellus sodales nibh lectus, at ullamcorper
+          dui tincidunt vel.
+        </h2>
+        <img
+          width="300"
+          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f416da27-6d39-4f69-9774-51542a594a98/d88sr5c-9e2dc95c-0f04-43d6-9816-2e225e745af2.png/v1/fill/w_1024,h_1349,strp/books_png_by_violettalestrange_d88sr5c-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM0OSIsInBhdGgiOiJcL2ZcL2Y0MTZkYTI3LTZkMzktNGY2OS05Nzc0LTUxNTQyYTU5NGE5OFwvZDg4c3I1Yy05ZTJkYzk1Yy0wZjA0LTQzZDYtOTgxNi0yZTIyNWU3NDVhZjIucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.SvN9SxH3bScwrSJTVPzOteUtNekHPF9GwPBOebbdSkI"
+        />
+      </div>
+      <footer className="outline bg-white mt-10">
+        <h3 className="text-xl p-6 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cuando la
+          espada sigue en pie en el camino del cabllero, el herrero y el heraldo
+          de la muerte.
+        </h3>
+      </footer>
+    </>
+  );
+}
+
+export default Home;
