194fa67da65cc6c98dcaa71b6811b0c271a2c92d
Nicolas Orozco <nicolasorozcodavila@gmail.com>
2023-01-14 23:59:16 -0500

    Add correct paddings and margins

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="bg-base-300 m-auto flex flex-col items-center max-w-4xl border-b-4 border-x-4 border-black">
+      <main className="bg-base-300 m-auto flex flex-col items-center max-w-4xl border-b-4 border-x-4 border-black py-8 px-8">
         {children}
       </main>
       <Footer />
